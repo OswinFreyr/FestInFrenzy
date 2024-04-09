f44b93d154502ff7c3fa5e4b460fb47079b22bcb
ines <bouadilin@gmail.com>
2024-04-09 16:17:27 +0200

    bug fix

--- FestInFrenzy_Front/components/FestivalComponent.vue
+++ FestInFrenzy_Front/components/FestivalComponent.vue
@@ -1,54 +1,35 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-const route = useRoute()
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
 
-let nomFestival, identifiantFestival, siteInternetFestival, emailFestival, sousCategorie, periode = ref("")
-let nomRegion, nomCommune, postalCodeCommune, nomDiscipline, latitude, longitude, zone = ref("");
-let festival = ref({})
+let festival = ref({});
+
 onMounted(async () => {
-  // Récupérer l'id de l'article à afficher dans la route
-  const festivalId = ref(route.params.id)
-  console.log(festivalId)
-  // Récupération de l'article ciblé
-  // let festivalAPI = await fetch(`adresseAPI/${festivalId.value}`)
-  let festivalAPI = await fetch ('/datasProvisoires/festivals.json');
-  // C'est un objet (ou un tableau), pas besoin d'utiliser .value
-  festival = await festivalAPI.json();
-  // C'est un type primitif (booléen, string ou number), il faut utiliser .value
-  // Récupération informations festival
-  nomFestival.value = festival.festivals.find(item => item.id === festivalId.value).nom;
-  identifiantFestival.value = festival.festivals.find(item => item.id === festivalId.value).identifiant;
-  siteInternetFestival.value = festival.festivals.find(item => item.id === festivalId.value).site_internet;
-  emailFestival.value = festival.festivals.find(item => item.id === festivalId.value).e_mail;
-  sousCategorie.value = festival.festivals.find(item => item.id === festivalId.value).sous_categorie;
-  periode.value = festival.festivals.find(item => item.id === festivalId.value).periode;
-  nomRegion.value = festival.festivals.find(item => item.id === festivalId.value).regions.nom;
-  nomCommune.value = festival.festivals.find(item => item.id === festivalId.value).communes.nom;
-  postalCodeCommune.value = festival.festivals.find(item => item.id === festivalId.value).communes.postal_code;
-  nomDiscipline.value = festival.festivals.find(item => item.id === festivalId.value).disciplines.nom;
-  latitude.value = festival.festivals.find(item => item.id === festivalId.value).localisations.latitude;
-  longitude.value = festival.festivals.find(item => item.id === festivalId.value).localisations.longitude;
-  zone.value = festival.festivals.find(item => item.id === festivalId.value).envergures.zone;
-})
+  const festivalId = route.params.id;
+
+  let festivalAPI = await fetch("/datasProvisoires/festivals.json");
+  const festivals = (await festivalAPI.json()).festivals;
+
+  festival.value = festivals.find((el) => el.id === festivalId);
+});
 </script>
 <template>
   <div>
-    <h1>{{nomFestival}}</h1>
+    <h1>{{ festival.nom }}</h1>
     <ul>
-      <li>{{ identifiantFestival }}</li>
-      <li>{{ siteInternetFestival }}</li>
-      <li>{{ emailFestival }}</li>
-      <li>{{ sousCategorie }}</li>
-      <li>{{ periode }}</li>
-      <li>{{ nomRegion }}</li>
-      <li>{{ nomCommune }}</li>
-      <li>{{ postalCodeCommune }}</li>
-      <li>{{nomDiscipline}}</li>
-      <li>{{ latitude }}</li>
-      <li>{{ longitude }}</li>
-      <li>{{ zone }}</li>
+      <li>{{ festival.id }}</li>
+      <li>{{ festival.siteInternet }}</li>
+      <li>{{ festival.email }}</li>
+      <li>{{ festival.sousCategorie }}</li>
+      <li>{{ festival.periode }}</li>
+      <li>{{ festival.nomRegion }}</li>
+      <li>{{ festival.nomCommune }}</li>
+      <li>{{ festival.postalCodeCommune }}</li>
+      <li>{{ festival.nomDiscipline }}</li>
+      <li>{{ festival.latitude }}</li>
+      <li>{{ festival.longitude }}</li>
+      <li>{{ festival.zone }}</li>
     </ul>
   </div>
-  
 </template>
