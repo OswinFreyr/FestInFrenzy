2978c55c50c1eebe0e307c3d2ce3fc203c1ffb2a
ines <bouadilin@gmail.com>
2024-04-11 16:43:58 +0200

    afficher seulement si tout est chargé

--- FestInFrenzy_Front/components/FestivalComponent.vue
+++ FestInFrenzy_Front/components/FestivalComponent.vue
@@ -1,43 +1,5 @@
-<script setup>
-import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
-const route = useRoute();
-
-let festival = ref({});
-let region = ref({});
-let commune = ref({});
-onMounted(async () => {
-  const festivalId = route.params.id;
-  // récupération festival
-  let festivalAPI = await fetch(
-    `http://10.3.211.68:2000/api/v1/festivals/${festivalId}`
-  );
-  festival.value = await festivalAPI.json();
-  // recupération région
-  const regionApi = await fetch(
-    `http://10.3.211.68:2000/api/v1/regions/${festival.value.regionId}`
-  );
-  region.value = await regionApi.json();
-  region.value.nom = region.value.nom
-    ? region.value.nom
-    : "Région non renseignée.";
-  // recupération commune
-  const communeApi = await fetch(
-    `http://10.3.211.68:2000/api/v1/communes/${festival.value.communeId}`
-  );
-  commune.value = await communeApi.json();
-  commune.value.nom = commune.value.nom
-    ? commune.value.nom
-    : "Commune non renseignée.";
-  // verif mail
-  festival.value.e_mail = festival.value.e_mail
-    ? `<a href="mailto:${festival.value.e_mail}" target="_blank">Contact</a>`
-    : "Adresse email non renseignée.";
-
-});
-</script>
 <template>
-  <div>
+  <div v-if="!isLoading">
     <ul style="display: flex; flex-direction: column; align-items: center">
       <li>
         <h1>{{ festival.nom }}</h1>
@@ -46,12 +8,49 @@ onMounted(async () => {
     </ul>
     <ul>
       <li>
-        <a href="{{ festival.site_internet }}" target="_blank"> Site Web</a>
+        <a :href="festival.site_internet" target="_blank"> Site Web</a>
       </li>
       <li v-html="festival.e_mail"></li>
       <li>{{ festival.sous_categorie }}</li>
       <li>{{ region.nom }}, {{ commune.nom }}</li>
     </ul>
   </div>
+  <div v-else>
+    <p>Chargement en cours...</p>
+  </div>
 </template>
 
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+
+let festival = ref({});
+let region = ref({});
+let commune = ref({});
+let isLoading = ref(true); 
+
+onMounted(async () => {
+  const festivalId = route.params.id;
+  // Récupération du festival
+  let festivalAPI = await fetch(
+    `http://10.3.211.68:2000/api/v1/festivals/${festivalId}`
+  );
+  festival.value = await festivalAPI.json();
+  // Récupération de la région
+  const regionApi = await fetch(
+    `http://10.3.211.68:2000/api/v1/regions/${festival.value.regionId}`
+  );
+  region.value = await regionApi.json();
+  region.value.nom = region.value.nom ? region.value.nom : "Région non renseignée.";
+  // Récupération de la commune
+  const communeApi = await fetch(
+    `http://10.3.211.68:2000/api/v1/communes/${festival.value.communeId}`
+  );
+  commune.value = await communeApi.json();
+  commune.value.nom = commune.value.nom ? commune.value.nom : "Commune non renseignée.";
+  // Vérification de l'e-mail
+  festival.value.e_mail = festival.value.e_mail ? `<a href="mailto:${festival.value.e_mail}" target="_blank">Contact</a>` : "Adresse email non renseignée.";
+  isLoading.value = false;
+});
+</script>
--- FestInFrenzy_Front/pages/HomeView.vue
+++ FestInFrenzy_Front/pages/HomeView.vue
@@ -1,23 +1,122 @@
+<template>
+  <div>
+    <Header />
+    <main class="main" >
+      <ULandingSection>
+        <div class="relative">
+          <img
+            src="https://cdn.discordapp.com/attachments/1227220701426090045/1227917582045151293/concert-8282026_1280.jpg?ex=662a265d&is=6617b15d&hm=33f8d58ed3c16a341cbc123f6c2518df543a59109586918f2836573f9258de7f&"
+            class="w-2/3 rounded-md shadow-x3 ring-1 ring-gray-300 dark:ring-gray-700"
+          />
+          <div
+            class="absolute inset-0 flex flex-col justify-center items-center text-center p-6"
+          >
+            <i class="text-6xl mb-4 text-white i-heroicons-sparkles"></i>
+            <h1
+              class="ext-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl lg:text-5xl"
+            >
+              FestInFrenzy
+            </h1>
+            <p class="mt-6 text-lg/8 text-gray-600 dark:text-gray-300">
+              Il n'a jamais été aussi simple de chercher des festivals ! 🪩
+            </p>
+          </div>
+        </div>
+      </ULandingSection>
+      <FestivalsSection v-if="!isLoading">
+        <section>
+          <div class="enTete">
+            <div class="relative pl-8 text-primary font-bold">
+              Festivals du moment
+            </div>
+            <UButton label="Voir plus" color="gray" class="voirPlus">
+              <template #trailing>
+                <UIcon
+                  name="i-heroicons-arrow-right-20-solid"
+                  class="w-5 h-5"
+                />
+              </template>
+            </UButton>
+          </div>
+          <CarouselComponent :festivalsList="festivalsList" />
+        </section>
+        <section>
+          <div class="enTete">
+            <div class="relative pl-8 text-primary font-bold">
+              Vos coups de coeur
+            </div>
+            <UButton label="Voir plus" color="gray" class="voirPlus">
+              <template #trailing>
+                <UIcon
+                  name="i-heroicons-arrow-right-20-solid"
+                  class="w-5 h-5"
+                />
+              </template>
+            </UButton>
+          </div>
+          <CarouselComponent :festivalsList="festivalsList" />
+        </section>
+        <section>
+          <div class="enTete">
+            <div class="relative pl-8 text-primary font-bold">
+              Spectacles vivants
+            </div>
+            <UButton label="Voir plus" color="gray" class="voirPlus">
+              <template #trailing>
+                <UIcon
+                  name="i-heroicons-arrow-right-20-solid"
+                  class="w-5 h-5"
+                />
+              </template>
+            </UButton>
+          </div>
+          <CarouselComponent
+            :festivalsList="disciplineSpectacleDeRueFestivalsList"
+          />
+        </section>
+        <section>
+          <div class="enTete">
+            <div class="relative pl-8 text-primary font-bold">Cinéma</div>
+            <UButton label="Voir plus" color="gray" class="voirPlus">
+              <template #trailing>
+                <UIcon
+                  name="i-heroicons-arrow-right-20-solid"
+                  class="w-5 h-5"
+                />
+              </template>
+            </UButton>
+          </div>
+          <CarouselComponent :festivalsList="disciplineCinemaFestivalsList" />
+        </section>
+      </FestivalsSection>
+      <div v-else>
+        <p>Chargement en cours...</p>
+      </div>
+    </main>
+    <Footer />
+  </div>
+</template>
+
 <script setup>
 import Header from "../components/HeaderComponent.vue";
 import Footer from "../components/FooterComponent.vue";
 
-const runtimeConfig = useRuntimeConfig()
+const runtimeConfig = useRuntimeConfig();
 
 let festivalsList = ref([]);
 let disciplineSpectacleDeRueFestivalsList = ref([]);
 let disciplineCinemaFestivalsList = ref([]);
 
-let festivalsUrl = ""
-
+let festivalsUrl = "";
+let isLoading = ref(true);
 
 onMounted(async () => {
-  festivalsUrl = runtimeConfig.public.apiUrl + "festivals"
-  // tous les festivals
+  festivalsUrl = runtimeConfig.public.apiUrl + "festivals";
+  // Tous les festivals
   const festivalsApi = await fetch(festivalsUrl);
   festivalsList.value = await festivalsApi.json();
 
-  // festivals spectacles de rue
+  // Festivals spectacles de rue
   const disciplineSpectacleDeRueFestivalsListApi = await fetch(
     "http://10.3.211.68:2000/api/v1/disciplines/3"
   );
@@ -26,7 +125,7 @@ onMounted(async () => {
   disciplineSpectacleDeRueFestivalsList.value =
     disciplineSpectacleDeRueFestivalsList.value.festivals;
 
-  // festivals cinéma
+  // Festivals cinéma
   const disciplineCinemaFestivalsListApi = await fetch(
     "http://10.3.211.68:2000/api/v1/disciplines/9"
   );
@@ -34,72 +133,11 @@ onMounted(async () => {
     await disciplineCinemaFestivalsListApi.json();
   disciplineCinemaFestivalsList.value =
     disciplineCinemaFestivalsList.value.festivals;
+
+  // Marque le chargement comme terminé
+  isLoading.value = false;
 });
 </script>
-
-<template>
-  <Header />
-  <main class="main">
-    <ULandingSection>
-    <div class="relative">
-      <img
-        src="https://cdn.discordapp.com/attachments/1227220701426090045/1227917582045151293/concert-8282026_1280.jpg?ex=662a265d&is=6617b15d&hm=33f8d58ed3c16a341cbc123f6c2518df543a59109586918f2836573f9258de7f&"
-        class="w-2/3 rounded-md shadow-x3 ring-1 ring-gray-300 dark:ring-gray-700"
-      />
-      <div class="absolute inset-0 flex flex-col justify-center items-center text-center p-6">
-        <!-- Icon -->
-        <i class="text-6xl mb-4 text-white i-heroicons-sparkles"></i>
-        <!-- Title -->
-        <h1 class="ext-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl lg:text-5xl">FestInFrenzy</h1>
-        <!-- Description -->
-        <p class="mt-6 text-lg/8 text-gray-600 dark:text-gray-300">Il n'a jamais été aussi simple de chercher des festivals ! 🪩</p>
-      </div>
-    </div>
-  </ULandingSection>
-    <section>
-      <div class="enTete">
-        <div class="relative pl-8 text-primary font-bold">Festivals du moment</div>
-        <UButton label="Voir plus" color="gray" class="voirPlus">
-          <template #trailing>
-          <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
-          </template>
-        </UButton>          </div>
-      <CarouselComponent :festivalsList="festivalsList" />
-    </section>
-    <section>
-      <div class="enTete">
-        <div class="relative pl-8 text-primary font-bold">Vos coups de coeur</div>
-        <UButton label="Voir plus" color="gray" class="voirPlus">
-          <template #trailing>
-          <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
-          </template>
-        </UButton>          </div>
-      <CarouselComponent :festivalsList="festivalsList" />
-    </section>
-    <section>
-      <div class="enTete">
-        <div class="relative pl-8 text-primary font-bold">Spectacles vivants</div>
-        <UButton label="Voir plus" color="gray" class="voirPlus">
-          <template #trailing>
-          <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
-          </template>
-        </UButton>          </div>
-      <CarouselComponent :festivalsList="disciplineSpectacleDeRueFestivalsList" />
-    </section>
-    <section>
-      <div class="enTete">
-        <div class="relative pl-8 text-primary font-bold">Cinéma</div>
-        <UButton label="Voir plus" color="gray" class="voirPlus">
-          <template #trailing>
-          <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
-          </template>
-        </UButton>      
-      </div>
-      <CarouselComponent :festivalsList="disciplineCinemaFestivalsList" />
-    </section>
-  </main>
-  <Footer />
-</template>
 
 <style>
 .main {
