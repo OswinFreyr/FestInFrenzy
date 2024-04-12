0bde750dbf4031c4c0a7ef5a7e3771f986e8245b
Ahmed Amine SBAI <ahmed-amine.sbai@efrei.net>
2024-04-12 11:28:37 +0200

    FestivalCompo fini (sauf map)

--- FestInFrenzy_Front/components/FestivalComponent.vue
+++ FestInFrenzy_Front/components/FestivalComponent.vue
@@ -16,6 +16,10 @@ let regionsUrl = ""
 let communesUrl = ""
 let localisationsUrl = ""
 
+const formatMois = (mois) => {
+  return mois.map(m => m.nom).join(', ');
+};
+
 onMounted(async () => {
   festivalsUrl = runtimeConfig.public.apiUrl + "festivals"
   regionsUrl = runtimeConfig.public.apiUrl + "regions"
@@ -73,42 +77,24 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div v-if="!isLoading" >
-      <ul style="display: flex; flex-direction: column; align-items: center">
-        <li>
-          <h1>{{ festival.nom }}</h1>
-        </li>
-        <li><em>Identifiant :</em> {{ festival.identifiant }}</li>
-      </ul>
-      <ul>
-        <li>
-          <a href="{{ festival.site_internet }}" target="_blank"> Site Web</a>
-        </li>
-        <li v-html="festival.e_mail"></li>
-        <li>{{ festival.sous_categorie }}</li>
-        <li>{{ region.nom }}, {{ commune.nom }}</li>
-      </ul>
-      <div style="display: flex; flex-direction: row; justify-content: right; margin-top: -90px;">
-        <MapboxMap
-          map-id="<MAP_ID>"
-          style="position: relative; width: 300px; height: 300px; justify-content: right; "
-          :options="{
-            style: 'mapbox://styles/mapbox/outdoors-v12', // style URL
-            center: [localisation.latitude, localisation.longitude], // starting position
-            zoom: 10, // starting zoom
-            boxzoom: true
-          }"
-        >
-          <MapboxDefaultMarker 
-            marker-id="<MARKER_ID>"
-            :options="{compact: false}"
-            :lnglat="[localisation.latitude, localisation.longitude]"
-          >
-          </MapboxDefaultMarker>
-          <MapboxGeolocateControl />
-          <MapboxFullscreenControl />
-          <MapboxNavigationControl />
-        </MapboxMap>
-      </div>
+  <div class="flex items-center justify-center h-screen">
+  <ULandingSection v-if="!isLoading"
+    :title='festival.nom'
+    description="Informations sur le festival :"
+    :features="[
+      { name: 'Identifiant', description: festival.identifiant, icon: 'i-heroicons-identification' },
+      { name: 'Site Web', description: festival.site_internet, icon: 'i-heroicons-globe-alt' },
+      { name: 'Email', description: festival.e_mail, icon: 'i-heroicons-envelope' },
+      { name: 'Sous-catégorie', description: festival.sous_categorie, icon: 'i-heroicons-tag' },
+      { name: 'Localisation', description: `${region.nom}, ${commune.nom}`, icon: 'i-heroicons-map-pin' },
+      { name: 'Période', description: formatMois(festival.mois), icon: 'i-heroicons-map-pin' }
+    ]"
+    align="left"
+  >
+  <img
+      src="https://picsum.photos/360/640"
+      class="w-2/3 ml-auto rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
+    />
+    </ULandingSection>
   </div>
 </template>
