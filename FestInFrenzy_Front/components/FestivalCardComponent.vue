b22dd0459734c2567c4bf81a7ccdb9f055885591
Ahmed Amine SBAI <ahmed-amine.sbai@efrei.net>
2024-04-12 10:52:52 +0200

    Carousel Fix & Fav finis

--- FestInFrenzy_Front/components/FestivalCardComponent.vue
+++ FestInFrenzy_Front/components/FestivalCardComponent.vue
@@ -2,22 +2,31 @@
   <UPricingCard
     :title="festival.nom"
     :description="discipline.nom"
-    icon="i-simple-icons-tailwindcss"
+    icon="i-heroicons-cube"
     :to="{ name: 'festival', params: { id: festivalId } }"
     target="_blank"
     highlight
-    :badge="{ label: 'Date' }"
     :button="{ label: '', icon: 'i-heroicons-arrow-right-20-solid', onClick: redirectToFestival }"
     :features="[region.nom]"
     orientation="horizontal"
     align="bottom"
     style="padding: 30px"
-  />
+  >
+  <template v-slot:footer>
+      <button
+        @click="toggleFavorite(festivalId)"
+        class="favorite-button"
+      >
+      <i :class="iconFav"></i>
+      </button>
+    </template>
+  </UPricingCard>
 </template>
 
 
 <script setup>
 import { useRouter } from 'vue-router';
+import { toggleFavoriteFestival, isFavorite } from '~/utils/favorites.js';
 
 const runtimeConfig = useRuntimeConfig()
 
@@ -38,6 +47,21 @@ const redirectToFestival = () => {
   router.push({ name: "festival", params: { id: props.festivalId } });
 };
 
+function toggleFavorite(festivalId) {
+  toggleFavoriteFestival(festivalId);
+  setBadgeProperties();
+};
+
+const setBadgeProperties = () => {
+  if (isFavorite(props.festivalId)) {
+    iconFav.value = "i-heroicons-star-solid text-yellow-500"
+  } else {
+    iconFav.value = "i-heroicons-star text-yellow-500"
+  }
+};
+
+let iconFav = ref("");
+
 onMounted(async () => {
   disciplinesUrl = runtimeConfig.public.apiUrl + "disciplines";
   regionsUrl = runtimeConfig.public.apiUrl + "regions";
@@ -55,33 +79,25 @@ onMounted(async () => {
   region.value.nom = region.value.nom
     ? region.value.nom
     : "Région non renseignée.";
+
+    setBadgeProperties();
+});
+
+watchEffect(() => {
+  setBadgeProperties();
 });
 </script>
 
 <style scoped>
-.card {
-  background-color: white;
-  border-radius: 0.5rem;
-  color: #000;
-}
-
-.card-title {
-  font-size: 1.5rem;
-  margin-bottom: 0.5rem;
-}
-
-.card-body {
-  padding: 1rem;
-}
-
-.card-actions {
-  margin-top: 1rem;
-}
-
-.btn-primary {
-  color: white;
-  padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
+.favorite-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  display: flex;
+  gap: 4px;
+  padding: 8px;
+  font-size: 2rem;
+  color: inherit;
   cursor: pointer;
 }
 </style>
