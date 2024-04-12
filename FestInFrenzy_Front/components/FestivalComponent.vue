20d8007a08520cfd7f3406c793341c69915f4ec9
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2024-04-12 09:31:41 +0200

    Update FestivalComponent.vue

--- FestInFrenzy_Front/components/FestivalComponent.vue
+++ FestInFrenzy_Front/components/FestivalComponent.vue
@@ -1,31 +1,3 @@
-<template>
-  <div v-if="!isLoading">
-    <ul style="display: flex; flex-direction: column; align-items: center">
-      <li>
-        <h1>{{ festival.nom }}</h1>
-      </li>
-      <li><em>Identifiant :</em> {{ festival.identifiant }}</li>
-    </ul>
-    <ul>
-      <li>
-        <a :href="festival.site_internet" target="_blank"> Site Web</a>
-      </li>
-      <li v-html="festival.e_mail"></li>
-      <li>{{ festival.sous_categorie }}</li>
-      <li>{{ region.nom }}, {{ commune.nom }}</li>
-      <ul style="display: flex; flex-direction: row">
-        Période :&nbsp;
-        <li v-for="(mois, index) in festival.mois" :key="index">
-          {{ index !== 0 ? ", " : "" }}{{ mois.nom }}
-        </li>
-      </ul>
-    </ul>
-  </div>
-  <div v-else>
-    <p style="text-align: center">Chargement en cours...</p>
-  </div>
-</template>
-
 <script setup>
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
@@ -34,8 +6,6 @@ const runtimeConfig = useRuntimeConfig()
 
 const route = useRoute();
 
-const runtimeConfig = useRuntimeConfig();
-const route = useRoute();
 let festival = ref({});
 let region = ref({});
 let commune = ref({});
