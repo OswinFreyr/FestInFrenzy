119041d3f43b7260e148a34679992c9a4b20e2c7
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2024-04-12 11:46:18 +0200

    annuaire fini

--- FestInFrenzy_Front/pages/EventsView.vue
+++ FestInFrenzy_Front/pages/EventsView.vue
@@ -1,22 +1,18 @@
-<script setup>
+<!-- <script setup>
 import { useRouter } from 'vue-router';
 import { ref, onMounted, defineProps } from 'vue';
 
 const router = useRouter();
 const runtimeConfig = useRuntimeConfig();
 
-const props = defineProps({
-  // festival: Object,
-  // festivalId: Number,
-});
 
 let isLoading = ref(true);
-let discipline = ref({});
-let region = ref({});
 let festivalObject = ref([])
 let festivalsList = ref([])
-let previousRoute = ref("")
-let nextRoute = ref("")
+
+let url = new URL(window.location.href);
+let params = new URLSearchParams(url.search);
+let pageID = ref() ? pageID.value : 1;
 
 let disciplinesUrl = ""
 let regionsUrl = ""
@@ -26,11 +22,11 @@ const redirectToFestival = () => {
   router.push({ name: 'festival', params: { id: props.festivalId } });
 };
 
-onMounted(async () => {
+onMounted(async (pageID) => {
   try {
     disciplinesUrl = runtimeConfig.public.apiUrl + 'disciplines'; 
     regionsUrl = runtimeConfig.public.apiUrl + 'regions'; 
-    limitedFestivalsUrl = runtimeConfig.public.apiUrl + "festivals/limit"
+    limitedFestivalsUrl = runtimeConfig.public.apiUrl + `festivals/limit/?pageId=${pageI}`
     
     const festivalApi = await fetch(limitedFestivalsUrl);
     festivalObject.value = await festivalApi.json()
@@ -59,7 +55,6 @@ async function nextPage() {
 }
 
 
-
 </script>
 
 <template>
@@ -82,4 +77,81 @@ async function nextPage() {
     </li>
   </ul>
 
+</template> -->
+
+<script setup>
+import { useRouter } from 'vue-router';
+import { ref, onMounted, defineProps } from 'vue';
+
+const router = useRouter();
+const runtimeConfig = useRuntimeConfig();
+
+const props = defineProps({
+  // festival: Object,
+  // festivalId: Number,
+});
+
+let isLoading = ref(true);
+let festivalObject = ref([])
+let festivalsList = ref([])
+
+let limitedFestivalsUrl = ref("")
+let fetchKey = 0
+
+const redirectToFestival = () => {
+  router.push({ name: 'festival', params: { id: props.festivalId } });
+};
+
+onMounted(async () => {
+  limitedFestivalsUrl.value = runtimeConfig.public.apiUrl + "festivals/limit"
+  await fetchData();
+});
+
+async function fetchData() {
+  try {
+    
+    const festivalApi = await fetch(limitedFestivalsUrl.value);
+    festivalObject.value = await festivalApi.json()
+    festivalsList.value = festivalObject.value.data
+    console.log(festivalsList.value);
+    fetchKey++;
+    isLoading.value = false;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error);
+  }
+}
+
+async function previousPage() {
+  limitedFestivalsUrl.value = festivalObject.value.previousUrl
+  console.log(limitedFestivalsUrl.value);
+  await fetchData();
+}
+
+async function nextPage() {
+  limitedFestivalsUrl.value = festivalObject.value.nextUrl
+  console.log(limitedFestivalsUrl.value);
+  await fetchData();
+}
+
+</script>
+
+<template>
+  <div v-if="isLoading">
+    Chargement en cours...
+  </div>
+  <div v-else >
+    <FestivalsListComponent :festivalsList="festivalsList" :key="fetchKey" />
+  </div>
+  <ul>
+    <li v-if="festivalObject.previousUrl">
+      <button @click="previousPage">
+        Page précédente
+      </button>
+    </li>
+    <li v-if="festivalObject.nextUrl">
+      <button @click="nextPage">
+        Page suivante
+      </button>
+    </li>
+  </ul>
 </template>
