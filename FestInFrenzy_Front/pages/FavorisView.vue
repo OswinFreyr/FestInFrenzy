c46293f7d0e4d4bf6d9847e674471a37a7bb1238
ines <bouadilin@gmail.com>
2024-04-12 10:09:08 +0200

    style titre paage favoris

--- FestInFrenzy_Front/pages/FavorisView.vue
+++ FestInFrenzy_Front/pages/FavorisView.vue
@@ -1,44 +1,54 @@
 <template>
-    <div>
-      <Header />
-      <h1>Favoris :)</h1>
-      <div v-if="favoriteFestivals().length === 0">
-        <p>Aucun festival favori pour le moment.</p>
-      </div>
-      <div v-else>
-        <template v-if="showListComponent">
-            <FestivalsListComponent :festivalsList="favoriteFestivalsDetails" />
-        </template>
-      </div>
-      <Footer />
+  <div>
+    <Header />
+    <h1
+      style="
+        text-transform: uppercase;
+        font-size: 28px;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        text-align: center;
+      "
+    >
+      Vos Festivals favoris
+    </h1>
+    <div v-if="favoriteFestivals().length === 0">
+      <p>Aucun festival favori pour le moment.</p>
     </div>
-  </template>
-  
-  <script setup>
-  import Header from "../components/HeaderComponent.vue";
-  import Footer from "../components/FooterComponent.vue";
-  import FestivalsListComponent from "../components/FestivalsListComponent.vue";
-  
-  import { favoriteFestivals } from '~/utils/favorites.js';
-  
-  const favoriteIds = favoriteFestivals();
-  const showListComponent = ref(false);
-  let favoriteFestivalsDetails = ref([]);
+    <div v-else>
+      <template v-if="showListComponent">
+        <FestivalsListComponent :festivalsList="favoriteFestivalsDetails" />
+      </template>
+    </div>
+    <Footer />
+  </div>
+</template>
 
-  onMounted(async () => {
-    await fetchFavoriteFestivalsDetails();
-      showListComponent.value = true;
-  });
-  
-  const fetchFavoriteFestivalsDetails = async () => {
-    const fetchedFestivals = [];
-    for (const festivalId of favoriteIds) {
-      const festivalApi = await fetch(`http://10.3.211.68:2000/api/v1/festivals/${festivalId}`);
-      const festivalData = await festivalApi.json();
-      fetchedFestivals.push(festivalData);
-    }
-    favoriteFestivalsDetails.value = fetchedFestivals;
-  };
-  
-  </script>
-  
+<script setup>
+import Header from "../components/HeaderComponent.vue";
+import Footer from "../components/FooterComponent.vue";
+import FestivalsListComponent from "../components/FestivalsListComponent.vue";
+
+import { favoriteFestivals } from "~/utils/favorites.js";
+
+const favoriteIds = favoriteFestivals();
+const showListComponent = ref(false);
+let favoriteFestivalsDetails = ref([]);
+
+onMounted(async () => {
+  await fetchFavoriteFestivalsDetails();
+  showListComponent.value = true;
+});
+
+const fetchFavoriteFestivalsDetails = async () => {
+  const fetchedFestivals = [];
+  for (const festivalId of favoriteIds) {
+    const festivalApi = await fetch(
+      `http://10.3.211.68:2000/api/v1/festivals/${festivalId}`
+    );
+    const festivalData = await festivalApi.json();
+    fetchedFestivals.push(festivalData);
+  }
+  favoriteFestivalsDetails.value = fetchedFestivals;
+};
+</script>
